19f4c184f1a7803687f34e0b74ce07e28e1e2020
DavideTimo <38298738+DavideTimo@users.noreply.github.com>
2026-03-03 15:30:04 +0000

    Correzioni varie

--- MBGroup.Web/obj/Debug/net8.0/MBGroup.Web.AssemblyInfo.cs
+++ MBGroup.Web/obj/Debug/net8.0/MBGroup.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MBGroup.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e181baf1c8455ac19366d90b33e1afc96b4f2db5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae314b02b7da668e1dd464e89ab731108a82199e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MBGroup.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MBGroup.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
